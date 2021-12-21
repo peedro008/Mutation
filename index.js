7968fb83a28cb904c60851a02e02ad16a4dd1a30
peedro008 <pedro39661@gmail.com>
2021-12-21 13:44:38 -0300

    readme

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const { sequelize } = require('./src/db')
 
 
 sequelize.sync({ force: true }).then(() => {
-  server.listen(3000, async()=>{
+  server.listen(8080, async()=>{
     
    
     try{
-      console.log('listening at 3000')
+      console.log('listening at 8080')
     }
     catch(e){
-      console.log("Error in sequelize.sinc, index.js: " + e)
+      console.log("Error in sequelize.sync, index.js: " + e)
     }
   });
 });
--- src/db.js
+++ src/db.js
@@ -3,7 +3,18 @@ const {postgre}=require("../env")
 const fs = require('fs');
 const path = require('path');
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(`postgres://${postgre}@localhost:5432/Mutation`, {logging: false,})
+
+
+//const sequelize = new Sequelize(`postgres://${postgre}@localhost:5432/Mutation`, {logging: false,})
+
+const sequelize = new Sequelize( "postgres", "postgres", "pesanmene",  {
+  host: "mutation.cnudap9ls8sm.us-east-2.rds.amazonaws.com",
+  dialect:'postgres',
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
+
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
